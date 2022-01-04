715ffc879eb351e41462ccdd6eb7ee904d1464de
JuhunC <a71790890@gmail.com>
2022-01-04 23:30:44 +0900

    Initial Commit

---
+++ CameraSealChecker/CameraSealCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraSealCheckerApp.swift
+//  CameraSealChecker
+//
+//  Created by JuhunChoi on 2022/01/04.
+//
+
+import SwiftUI
+
+@main
+struct CameraSealCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
